c66f9937b08ec63e9a64a797288d6c81683e6704
Lera <ohhsith@ukr.net>
2021-08-11 12:20:00 +0300

    js file fixed

--- js-script.js
+++ js-script.js
@@ -1,17 +1,16 @@
 
 class Clock {
     
-    constructor() {
+    constructor(timer) {
         this.showFullTime = true;
-        this.time = document.querySelector('.time');
+        this.time = document.querySelector(timer);
     }
     render() { 
         this.now = new Date();
         this.hours = this.now.getHours() < 10 ? '0' + this.now.getHours() : this.now.getHours();
         this.minutes = this.now.getMinutes() < 10 ? '0' + this.now.getMinutes() : this.now.getMinutes();
         this.seconds = this.now.getSeconds() < 10 ? '0' + this.now.getSeconds() : this.now.getSeconds(); 
-        // this.time.innerHTML=`${this.hours}:${this.minutes}:${this.seconds}`;
-        this.showFullTime ? this.time.innerHTML=`${this.hours}:${this.minutes}:${this.seconds}`: this.time.innerHTML=`${this.hours}:${this.minutes}`;
+        this.time.innerHTML = this.showFullTime ? `${this.hours}:${this.minutes}:${this.seconds}`: `${this.hours}:${this.minutes}`;
        
     }
     switchTimeFormat () {
@@ -21,8 +20,8 @@ class Clock {
     }
 }
 
-
-let clock = new Clock;
+      
+let clock = new Clock('.time');
 setInterval(() =>
     clock.render()
 , 100);
